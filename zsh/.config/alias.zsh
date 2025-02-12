dfe18cc0ce94f15fcfb1e3bf8f325a2226185237
Peter Goes <petergoes@gmail.com>
2025-02-12 10:49:54 +0100

    Add basic zsh config

---
+++ zsh/.config/alias.zsh
@@ -0,0 +1,11 @@
+alias ls='ls -lgha'
+alias ip="ipconfig getifaddr en0"
+alias https='http --default-scheme=https'
+alias github='gh'
+alias showHiddenFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideHiddenFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
---
+++ zsh/.zshrc
@@ -0,0 +1,2 @@
+# Load custom aliases
+source ~/.config/zsh/alias.zsh
