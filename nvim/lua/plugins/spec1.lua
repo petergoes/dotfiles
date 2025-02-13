afad948a7c2110471e956a3ce9e5dcc3ac129022
Peter Goes <petergoes@gmail.com>
2025-02-13 16:47:19 +0100

    make neovim transparent

--- nvim/lua/plugins/spec1.lua
+++ nvim/lua/plugins/spec1.lua
@@ -1,4 +1,5 @@
 return {
+    { "xiyaowong/transparent.nvim", lazy = false },
 	{ "folke/which-key.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{ "folke/tokyonight.nvim", lazy = false, priority = 1000 }
